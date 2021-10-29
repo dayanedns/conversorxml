393b1579d6615a6c36bd54998ef1ae874c094cf8
dayanedns <93356506+dayanedns@users.noreply.github.com>
2021-10-28 21:09:24 -0300

    Add files via upload

---
+++ rotas.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+
+ app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/dbBanco.js", function(req, res){
+  res.sendFile(__dirname + "/dbBanco.js");
+});
+app.get("/index.js", function(req, res){
+  res.sendFile(__dirname + "/index.js");
+});
+
+app.get("/bundle.js", function(req, res){
+  res.sendFile(__dirname + "/bundle.js");
+});
+ app.listen(8081, function(){
+  console.log("Servidor rodando url http://localhost:8081");
+});
