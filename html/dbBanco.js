a7b540de52089f85c5cb33aa2492d337f1360d69
Dayane Nunes da Silva <dayane.nunes@priner.com.br>
2021-11-02 16:26:16 -0300

    criado endpoint das clinicas

--- html/dbBanco.js
+++ html/dbBanco.js
@@ -1,75 +1,54 @@
 require('dotenv').config()
-/*import {
-    queryCPF
-  } from './index.js'*/
 
-  
-//var indexJS = require ('./index')
-var dbConn;
-
- var sql = require('mssql');
+var sql = require('mssql');
 
 
-//set up a sql server credentials
+// Cria configuração do mssql
 var config = {
-server: process.env.DB_SERVER||'localhost',
-database: process.env.DB_BASE||'projeto_exames',
-user: process.env.DB_USER||'',
-password: process.env.DB_PASS||'',
-port: process.env.DB_PORT||38000,
-synchronize: true,
-options: {
-  trustServerCertificate: true // change to true for local dev / self-signed certs
+	server: process.env.DB_SERVER || 'localhost',
+	database: process.env.DB_BASE || 'projeto_exames',
+	user: process.env.DB_USER || '',
+	password: process.env.DB_PASS || '',
+	port: process.env.DB_PORT || 38000,
+	synchronize: true,
+	options: {
+		trustServerCertificate: true // Confiar em certificado auto-assinado
+	}
 }
 
 
-};
+async function getCpfInfo(cpf) {
+	try {
+		await sql.connect(config);
 
- async function getCpfInfo(cpf){
-  try{
-    // dbConn = new sql.ConnectionPool(config);
-    await sql.connect(config);
+		const result = await sql.query(`select a.chapa, a.codpessoa from pfunc as a 
+		 inner join ppessoa as b on (a.codpessoa = b.CODIGO)
+		where b.cpf ='${cpf}'
+		and a.codsituacao <> 'D'`)
 
-    // console.log("conectado")
-    //  dbConn.query(`select a.chapa from pfunc as a 
-    //  inner join ppessoa as b on (a.codpessoa = b.CODIGO)
-    // where b.cpf ='${cpf}'
-    // and a.codsituacao <> 'D'`, function (err, recordset) {
-    // if (err) console.log(err)
-    // // send records as a response
-    
-    // dadosChapa = recordset.recordset[0].chapa;
-    // console.log(recordset)
-    
-    // //res.send(recordset);
-    // })
-    const result = await sql.query(`select a.chapa, a.codpessoa from pfunc as a 
-     inner join ppessoa as b on (a.codpessoa = b.CODIGO)
-    where b.cpf ='${cpf}'
-    and a.codsituacao <> 'D'`)
-
-    return await result.recordset[0]
+		return await result.recordset[0]
+	}
+	catch (error) {
+		throw error
+	}
 }
-  catch(error){
-    throw error
-  }
- }
- async function getClinicaInfo(cpf){
-  try{
-    
-    await sql.connect(config);
+async function getClinicasInfo() {
+	try {
 
-    const resultClinica = await sql.query(`select codentidade, razaosocial from ventidades
-    where codgrupoentidade = 1`)
+		await sql.connect(config);
 
-    return await result.recordset[0]
+		const resultClinicas = await sql.query(`select codentidade, razaosocial from ventidades
+		where codgrupoentidade = 1`)
+		console.log(resultClinicas.recordset)
+		return resultClinicas.recordset
+		
+	}
+	catch (error) {
+		throw error
+	}
 }
-  catch(error){
-    throw error
-  }
- }
- 
- module.exports = {
-   getCpfInfo,
-   getClinicaInfo
- }
+
+module.exports = {
+	getCpfInfo,
+	getClinicasInfo
+}
--- html/rotas.js
+++ html/rotas.js
@@ -1,52 +1,66 @@
-const express = require ('express');
-const app = express();
+const express = require('express')
+const app = express()
 const cors = require('cors')
 
-const { getCpfInfo } = require('./dbBanco')
+const { getCpfInfo, getClinicasInfo } = require('./dbBanco')
 
 app.use(cors())
 
 app.use(express.json())
 
- app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/dbBanco.js", function(req, res){
-  res.sendFile(__dirname + "/dbBanco.js");
+app.get("/dbBanco.js", function (req, res) {
+	res.sendFile(__dirname + "/dbBanco.js");
 });
-app.get("/index.js", function(req, res){
-  res.sendFile(__dirname + "/index.js");
-});
-
-app.get("/bundle.js", function(req, res){
-  res.sendFile(__dirname + "/bundle.js");
+app.get("/index.js", function (req, res) {
+	res.sendFile(__dirname + "/index.js");
 });
 
-app.get("/api/cpf/:cpf", async function (request, response){
-  const { cpf }  = request.params
+app.get("/bundle.js", function (req, res) {
+	res.sendFile(__dirname + "/bundle.js");
+});
 
-  // Grava na base o cpf capturado na requisição
+app.get("/api/cpf/:cpf", async function (request, response) {
+	const { cpf } = request.params
 
-  try{
-    const cpfInfo = await getCpfInfo(cpf)
+	// Grava na base o cpf capturado na requisição
 
-    if(!cpfInfo){
-      return response.status(400).json({
-        message: "CPF não encontrado"
-      })
-    }
+	try {
+		const cpfInfo = await getCpfInfo(cpf)
 
-    return response.json(cpfInfo)
-  }
-  catch(error){
-    return response.status(500).json({
-      message: `Erro ao buscar cpf`,
-      error
-    })
-  }
+		if (!cpfInfo) {
+			return response.status(400).json({
+				message: "CPF não encontrado"
+			})
+		}
+
+		return response.json(cpfInfo)
+	}
+	catch (error) {
+		return response.status(500).json({
+			message: `Erro ao buscar cpf`,
+			error
+		})
+	}
 })
 
-app.listen(8081, function(){
-  console.log("Servidor rodando url http://localhost:8081");
+app.get("/api/clinicas", async (request, response) => {
+	try{
+		const clinicas = await getClinicasInfo()
+
+		return response.json(clinicas)
+	}
+	catch(error){
+		return response.status(500).json({
+			message: "Não foi possivel encontrar as Clinicas",
+			error
+		})
+	}
+})
+
+app.listen(8081, function () {
+	console.log("Servidor rodando url http://localhost:8081");
 });
