0507d11404378e0d7095daa138bae13c506433c7
dayanedns <93356506+dayanedns@users.noreply.github.com>
2021-11-02 15:30:16 -0300

    Revert "Develop/rainan"

--- README.md
+++
@@ -1,4 +0,0 @@
-# Conversor de XML
-------------------------------------------------------
-
-[criar aqui descrição do que é, como instalar, funcionamento e etc do conversor de xml]
--- rotas.js
+++ rotas.js
@@ -1,15 +1,7 @@
 const express = require ('express');
-const cors = require('cors')
 const app = express();
 
-app.use(cors())
-/*
-* Serve os arquivos de front-end
-* o que for utilizado pela interface é exposto por aqui, nas respectivas rotas expostas
-*   Boa prática: mandar arquivos para uma pasta tipo /public ou /client,
-*   e servir esta pasta completa com app.use() (cheque documenação do express sobre como usar: https://expressjs.com/en/4x/api.html#app.use) 
-*/
-app.get("/", function(req, res){
+ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
@@ -23,29 +15,6 @@ app.get("/index.js", function(req, res){
 app.get("/bundle.js", function(req, res){
   res.sendFile(__dirname + "/bundle.js");
 });
-
-/*
-* Rotas da api:
-* Aqui que irão as rotas que retornam informações do servidor e por onde o servidor recebe informações do client, usando a arquitetura de REST API
-*/ 
-app.get('/api/hello', (request, response)=>{
-  // Acesse usando a rota: http://localhost:8080/api/hello/
-
-  return response.json({
-    message: `Olá Daiane!`
-  })
-})
-
-app.post('/api/cpf', (request, response)=>{
-  const { cpf } = request.body
-
-  // Grava na base o cpf capturado na requisição
-
-  return response.json({
-    cpf
-  })
-})
-
-app.listen(8080, function(){
-  console.log("Servidor rodando url http://localhost:8080");
+ app.listen(8081, function(){
+  console.log("Servidor rodando url http://localhost:8081");
 });
